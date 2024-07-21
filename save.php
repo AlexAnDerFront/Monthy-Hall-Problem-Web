bee777a15ec4a1effd156b215c3f6114f833997a
Alexanderfront <alexanderfront@gmail.com>
2024-07-21 15:42:12 +0200

    added database saveing capabilties.

--- save.php
+++ save.php
@@ -11,3 +11,14 @@ echo "jsLose: ". $jsLose. " ";
 $myfile = fopen("total-score.txt", "a");
 fwrite($myfile, $jsWin . "-" . $jsLose . PHP_EOL);
 fclose($myfile); 
+
+
+$db = new SQLite3('mysqlitedb.db');
+
+$db->exec('CREATE TABLE gameData (win INT, lose INT)'); // erstellt Tabelle
+$db->exec("INSERT INTO gameData (win) VALUES ('" . $jsWin . "')");
+$db->exec("INSERT INTO gameData (lose) VALUES ('" . $jsLose . "')");
+
+$result = $db->query('SELECT * FROM gameData');
+var_dump($result->fetchArray());
+ ?>
