8ad136b781f1196513e572efe45ca6344a545e98
Alexanderfront <alexistamarbeiten@gmail.com>
2024-07-22 13:06:35 +0200

    added comments

--- save.php
+++ save.php
@@ -1,5 +1,5 @@
 <?php
-
+// use http request to POST Js Data to PHP variables
 if(isset($_POST['jsWin' ]) && isset($_POST['jsLose'])) {
     $id = $_POST['id'];
     $jsWin= $_POST['jsWin'];
@@ -14,7 +14,7 @@ $myfile = fopen("total-score.txt", "a");
 fwrite($myfile, $jsWin . "-" . $jsLose . PHP_EOL);
 fclose($myfile); 
 
-
+// connect to database
 $db = new SQLite3('mysqlitedb.db');
 
 $db->exec('CREATE TABLE gameData (id TEXT, win INT, lose INT, numSwitch BOOLEAN)'); // erstellt Tabelle
