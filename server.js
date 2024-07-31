18b8342775699d3fa723bd732ad4c08d33affe08
Alexanderfront <alexistamarbeiten@gmail.com>
2024-07-31 17:14:53 +0200

    changed server.js to admit index.php as index.html

--- server.js
+++ server.js
@@ -1,79 +1,39 @@
-const fs = require('fs');
-const sqlite3 = require('sqlite3').verbose();
-const express = require('express');
-const bodyParser = require('body-parser');
+import { readFile, stat } from 'fs/promises';
 
-const app = express();
-app.use(bodyParser.json());
+Bun.serve({
+  
+  /** @param { Request } request */
+  async fetch(request) {
+    const url = new URL(request.url);
 
-function consoleLog(text) {
-  console.log(text);
-}
+    console.log(`${ request.method } ${ url.pathname }`);
 
-app.post('/updateData', (req, res) => {
-  const { id, jsWins, jsLoses, jsNumSwitch, jsWS, jsLS, jsWnS, jsLnS } = req.body;
+    if (url.pathname == '/' || url.pathname == '/index.php') {
+      const file = await readFile('./index.php', { encoding: 'utf-8' });
 
-  consoleLog(jsWS);
-  consoleLog(jsLS);
-  consoleLog(jsWnS);
-  consoleLog(jsLnS);
-
-  const db = new sqlite3.Database('singleGames.db');
-
-  db.run(`INSERT INTO gameData (id, wins, loses, numSwitch, WS, LS, WnS, LnS) 
-          VALUES (?, ?, ?, ?, ?, ?, ?, ?)`, 
-    [id, jsWins, jsLoses, jsNumSwitch, jsWS, jsLS, jsWnS, jsLnS], 
-    (err) => {
-      if (err) {
-        console.error(err.message);
-        res.status(500).send('Error inserting data');
-      } else {
-        res.send('Data inserted successfully');
-      }
-    }
-  );
-
-  db.close();
-});
-
-app.get('/getData', (req, res) => {
-  const db = new sqlite3.Database('singleGames.db');
-
-  const query = `
-    SELECT
-      COUNT(*) AS total_games,
-      SUM(CASE WHEN WS = 1 AND numSwitch = 1 THEN 1 ELSE 0 END) AS WS,
-      SUM(CASE WHEN LS = 1 AND numSwitch = 1 THEN 1 ELSE 0 END) AS LS,
-      SUM(CASE WHEN WnS = 1 AND numSwitch = 0 THEN 1 ELSE 0 END) AS WnS,
-      SUM(CASE WHEN LnS = 1 AND numSwitch = 0 THEN 1 ELSE 0 END) AS LnS,
-      CAST(SUM(CASE WHEN WS = 1 AND numSwitch = 1 THEN 1 ELSE 0 END) AS FLOAT) /
-        NULLIF(SUM(CASE WHEN (WS = 1 OR LS = 1) AND numSwitch = 1 THEN 1 ELSE 0 END), 0) AS WinrateSwitch,
-      CAST(SUM(CASE WHEN WnS = 1 AND numSwitch = 0 THEN 1 ELSE 0 END) AS FLOAT) /
-        NULLIF(SUM(CASE WHEN (WnS = 1 OR LnS = 1) AND numSwitch = 0 THEN 1 ELSE 0 END), 0) AS WinratenoSwitch
-    FROM gameData
-  `;
-
-  db.get(query, (err, row) => {
-    if (err) {
-      console.error(err.message);
-      res.status(500).send('Error fetching data');
-    } else {
-      res.json(row);
-
-      fs.writeFile('data.json', JSON.stringify(row), (err) => {
-        if (err) {
-          consoleLog("Error creating JSON file");
-        } else {
-          consoleLog("JSON file created successfully");
+      return new Response(file, {
+        headers: {
+          'content-type': 'text/html'
         }
       });
     }
-  });
 
-  db.close();
-});
+    if (await stat('.' + url.pathname).catch(() => false)) {
+      const extension = url.pathname.split('.').at(-1);
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+      const map = {
+        'js': 'application/javascript',
+        'css': 'text/css',
+        'jpeg': 'image/jpeg',
+      }
+
+      return new Response(await readFile('.' + url.pathname), {
+        headers: {
+          'content-type': map[extension]
+        }
+      });
+    }
+    
+    return new Response("Hello, World!");
+  }
+})
